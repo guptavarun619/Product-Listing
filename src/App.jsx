db55b3045957d5ddab5e64500aa74e92f7f03814
guptavarun619 <guptavarun619@gmail.com>
2023-01-11 22:32:02 +0530

    🚀 Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="py-2 text-center text-3xl font-bold ">
+        Product Listing : Initial Setup 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
